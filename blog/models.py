b73c547b9a9bde08da14cd4f066898e16e723dde
RISMME <python@istruly.sexy>
2018-07-05 22:24:26 +0900

    DONE: complete djangogirls tutorial

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     )
 
     def publish(self):
-        self.published_date = timezone.now
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
